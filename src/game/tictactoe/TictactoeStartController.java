f66a98383cd50bc9fc7fc99741b8898fd43c88f1
sungtaekoh <ohbass95@gmail.com>
2021-09-15 14:32:40 +0900

    tictactoeStart기본설정

---
+++ src/game/tictactoe/TictactoeStartController.java
@@ -0,0 +1,30 @@
+package game.tictactoe;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+
+public class TictactoeStartController implements Initializable{
+	Parent root;
+	public void setRoot(Parent root) {
+		this.root = root;
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+
+	}
+	public void pauseStartProc() {
+		
+	}
+	public void abstentionProc() {
+		
+	}
+	public void goMainProc() {
+		
+	}
+
+
+}
