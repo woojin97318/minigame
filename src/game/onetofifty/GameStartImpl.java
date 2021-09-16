0f29fb85df9a5d42a97bf0f0159fcde608534a09
woojin97318 <woojin97318@naver.com>
2021-09-16 13:32:38 +0900

    다음숫자 표시, 게임시작버튼 기능구현

--- src/game/onetofifty/GameStartImpl.java
+++ src/game/onetofifty/GameStartImpl.java
@@ -5,13 +5,16 @@ import java.util.Random;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class GameStartImpl implements GameStart {
 	Parent root;
-	Button[] btn = new Button[25];
-	int[] num = new int[50];
-	int step = 1;
-	int i = 25;
+	Button[] btn = new Button[25];	// 25개의 버튼을 저장할 배열
+	int[] num = new int[50];		// 1~50까지의 수가 들어가는 배열
+	Button gameStartBtn;			// 게임 시작 버튼
+	Label nextNumLb;				// 다음에 선택해야할 숫자 표시라벨
+	int step = 1;					// 게임 진행 시 선택해야할 숫자
+	int i = 25;						// 1~25 숫자버튼 클릭 시 다음 26~50숫자를 가져오기 위한 변수
 
 	@Override
 	public void setRoot(Parent root) {
@@ -19,17 +22,26 @@ public class GameStartImpl implements GameStart {
 	}
 
 	@Override
-	public void gameStart() {
+	public void gameStart() {	// 게임 시작 버튼 클릭 메소드
+		gameStartBtn = (Button)root.lookup("#gameStartBtn");
+		nextNumLb = (Label)root.lookup("#nextNumLb");
+		if(gameStartBtn.getText().equals("다시 시작"))	// 게임을 재시작시 step를 1로 초기화
+			step = 1;
+		else	// 첫 게임 시작시 시작버튼 클릭시 "다시 시작으로 setText"
+			gameStartBtn.setText("다시 시작");
+		
 		for (int i = 0; i < 50; i++)
 			num[i] = i + 1;	// 1~50 숫자 num배열에 초기화
+		
 		shakeNumber();	// num 배열의 숫자를 섞음
 
 		for (int i = 0; i < 25; i++) {        // 1 ~ 25 버튼에
 			btn[i] = (Button)root.lookup("#btn" + Integer.toString(i+1));
 			btn[i].setText(Integer.toString(num[i])); // num[0~24] 숫자를 입력
 		}
+		nextNumLb.setText(Integer.toString(step));	// 다음 눌러야할 숫자를 Label에 표시
 	}
-	public void shakeNumber() {		// 1 ~ 50 숫자를 뒤섞는 메소드
+	public void shakeNumber() {	// 1 ~ 50 숫자를 뒤섞는 메소드
 		Random r = new Random();
 		int x = 0, y = 0, t = 0;
 		for (int i = 0; i < 100; i++) {
@@ -49,7 +61,7 @@ public class GameStartImpl implements GameStart {
 	}
 
 	@Override
-	public void onClick(ActionEvent event) {	// 버튼 클릭시 이벤트 처리
+	public void onClick(ActionEvent event) {	// 게임 진행시 숫자버튼 클릭 메소드
 		if(step == Integer.parseInt(((Button)event.getSource()).getText())) {
 			if(step > 25) {
 				((Button)event.getSource()).setText("");
@@ -57,6 +69,7 @@ public class GameStartImpl implements GameStart {
 				((Button)event.getSource()).setText(Integer.toString(num[i++]));
 			}
 			step++;
+			nextNumLb.setText(Integer.toString(step));
 			if(step == 51) {	// 게임종료 및 타이머 종료
 				
 			}
